356dfcbc46574c36ef44c7465be28d0a9c60ed4f
narrat32 <34573092+narrat32@users.noreply.github.com>
2019-09-24 14:55:02 +1200

    Add files via upload

---
+++ JS/main.js
@@ -0,0 +1,10 @@
+/**First Slider**/
+$(".slider-one").not(".slick-initialized").slick({
+    
+    autoplay: true,
+    autoplaySpeed: 3000,
+    dots: true,
+    nextArrow: ".site-slider.slider-btn.next",
+    prevArrow: ".site-slider.slider-btn.previous"
+    
+});
