1b1325026f52ee4546accff6ce9daf9cf868393e
narrat32 <34573092+narrat32@users.noreply.github.com>
2019-09-24 15:42:25 +1200

    Add files via upload

--- nav.php
+++ nav.php
@@ -3,7 +3,7 @@
     <div class="collapse navbar-collapse" id="collapsibleNavId">
         <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
             <li class="nav-item ">
-                <a class="nav-link" href="index.php">Home</a>
+                <a class="nav-link" href="index.html">Home</a>
             </li>
             <li class="nav-item active">
                 <a class="nav-link" href="products.php">Trees</a>
@@ -21,9 +21,9 @@
                 <a class="nav-link" href="#">Help</a>
             </li>
         </ul>
-        <form class="form-inline my-2 my-lg-0" action="searchshop.php" method="GET">
+        <form action="searchshop.php" method="GET" class="form-inline my-2 my-lg-0">
             <input class="form-control mr-sm-2" type="text" placeholder="Search" name="search">
-            <button class="btn btn-warning my-2 my-sm-0" type="submit">Search</button>
+            <input class="btn btn-warning my-2 my-sm-0" type="submit">Search</input>
         </form>
         <ul class="navbar-nav mr-right mt-2 mt-lg-0">
             <li class="nav-item">
