7ce14ae80c37d445365c2d0e72481f26887272dc
98mbohra <49011759+98mbohra@users.noreply.github.com>
2019-09-25 22:24:51 +1200

    Update banner.php

--- banner.php
+++ banner.php
@@ -1,5 +1,5 @@
 <div class="Banner text-center">
     <br>
-    <img src="Assets/Logos/PlantATree.png" class="img-fluid banner-image" alt="Responsive image">
+    <img src="Assets/Logos/PlantATree.png" class="img-fluid banner-image width:50%" alt="Responsive image">
     <br><br>
 </div>
