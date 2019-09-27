e14b53755563d1485ae214a56f3be0e2cb7bbc8e
narrat32 <34573092+narrat32@users.noreply.github.com>
2019-09-27 13:44:49 +1200

    Add files via upload

--- item.php
+++ item.php
@@ -107,7 +107,7 @@ $itemListing = $db_handle->runQuery("SELECT * FROM tree WHERE Code='" . $_GET["C
             </div>
             <div class="cart-panel mt-3">
                 <input type="submit" class="btnAddAction btn btn-lg btn-primary mr-1 ml-1">Add to Wheelbarrow</button>
-                <button type="button" class="btn btn-primary btn-block btn-lg" onclick="cart.php">Go to Checkout</button>
+                <button type="button" class="btn btn-primary btn-block btn-lg" onclick="location.href = 'cart.php';">Go to Checkout</button>
             </div>
         </div>
     </div>
