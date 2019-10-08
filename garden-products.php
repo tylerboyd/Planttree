ecbefa221fb71e16ff45a763dc64730375470d31
tylerboyd <36950595+tylerboyd@users.noreply.github.com>
2019-10-08 16:17:28 +1300

    Add files via upload

--- garden-products.php
+++ garden-products.php
@@ -6,7 +6,7 @@ if (!empty($_GET["action"])) {
 	switch ($_GET["action"]) {
 		case "add":
 			if (!empty($_POST["quantity"])) {
-				$productByCode = $db_handle->runQuery("SELECT * FROM tree WHERE Code='" . $_GET["Code"] . "'");
+				$productByCode = $db_handle->runQuery("SELECT * FROM gardenproducts WHERE Code='" . $_GET["Code"] . "'");
 				$itemArray = array($productByCode[0]["Code"] => array('Name' => $productByCode[0]["Name"], 'Code' => $productByCode[0]["Code"], 'quantity' => $_POST["quantity"], 'Price' => $productByCode[0]["Price"], 'Image' => $productByCode[0]["Image"]));
 
 				if (!empty($_SESSION["cart_item"])) {
@@ -91,7 +91,7 @@ if (!empty($_GET["action"])) {
 
 				<h3 class="mt-2">Filters</h3>
 
-				<div class="dropdown">
+				<div class="dropdown mb-3">
 					<button class="btn btn-secondary dropdown-toggle btn-block" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
 						Select Categories
 					</button>
@@ -99,27 +99,84 @@ if (!empty($_GET["action"])) {
 						<!-- Can probably put a loop here for categories -->
 						<div class="dropdown-item">
 							<input type="checkbox" class="form-check-input" id="exampleCheck1">
-							<label class="form-check-label" for="exampleCheck1">Fruit Trees</label>
+							<label class="form-check-label" for="exampleCheck1">Maintenance Tools</label>
 						</div>
 						<div class="dropdown-item">
 							<input type="checkbox" class="form-check-input" id="exampleCheck1">
-							<label class="form-check-label" for="exampleCheck1">Hedges</label>
+							<label class="form-check-label" for="exampleCheck1">Digging Tools</label>
 						</div>
 						<div class="dropdown-item">
 							<input type="checkbox" class="form-check-input" id="exampleCheck1">
-							<label class="form-check-label" for="exampleCheck1">Evergreen Trees</label>
+							<label class="form-check-label" for="exampleCheck1">Cutting Tools</label>
 						</div>
 						<div class="dropdown-item">
 							<input type="checkbox" class="form-check-input" id="exampleCheck1">
-							<label class="form-check-label" for="exampleCheck1">NZ Native</label>
+							<label class="form-check-label" for="exampleCheck1">Garden Transport</label>
 						</div>
 						<div class="dropdown-item">
 							<input type="checkbox" class="form-check-input" id="exampleCheck1">
-							<label class="form-check-label" for="exampleCheck1">Gum Trees</label>
+							<label class="form-check-label" for="exampleCheck1">Lawn Care</label>
 						</div>
 						<div class="dropdown-item">
 							<input type="checkbox" class="form-check-input" id="exampleCheck1">
-							<label class="form-check-label" for="exampleCheck1">Palm Trees</label>
+							<label class="form-check-label" for="exampleCheck1">Apparel</label>
+						</div>
+						<div class="dropdown-item">
+							<input type="checkbox" class="form-check-input" id="exampleCheck1">
+							<label class="form-check-label" for="exampleCheck1">Fertiliser</label>
+						</div>
+					</div>
+				</div>
+
+				<div class="dropdown mt-3 mb-2">
+					<button class="btn btn-secondary dropdown-toggle btn-block" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+						Select Brand
+					</button>
+					<div class="dropdown-menu btn-block" aria-labelledby="dropdownMenuButton">
+						<!-- Can probably put a loop here for Brands -->
+						<div class="dropdown-item">
+							<input type="checkbox" class="form-check-input" id="exampleCheck1">
+							<label class="form-check-label" for="exampleCheck1">Stanley</label>
+						</div>
+						<div class="dropdown-item">
+							<input type="checkbox" class="form-check-input" id="exampleCheck1">
+							<label class="form-check-label" for="exampleCheck1">Atlas Trade</label>
+						</div>
+						<div class="dropdown-item">
+							<input type="checkbox" class="form-check-input" id="exampleCheck1">
+							<label class="form-check-label" for="exampleCheck1">Spear and Jackson</label>
+						</div>
+						<div class="dropdown-item">
+							<input type="checkbox" class="form-check-input" id="exampleCheck1">
+							<label class="form-check-label" for="exampleCheck1">Saxon</label>
+						</div>
+						<div class="dropdown-item">
+							<input type="checkbox" class="form-check-input" id="exampleCheck1">
+							<label class="form-check-label" for="exampleCheck1">Bacho</label>
+						</div>
+						<div class="dropdown-item">
+							<input type="checkbox" class="form-check-input" id="exampleCheck1">
+							<label class="form-check-label" for="exampleCheck1">Fiskars</label>
+						</div>
+						<div class="dropdown-item">
+							<input type="checkbox" class="form-check-input" id="exampleCheck1">
+							<label class="form-check-label" for="exampleCheck1">Hart</label>
+						</div>
+						<div class="dropdown-item">
+							<input type="checkbox" class="form-check-input" id="exampleCheck1">
+							<label class="form-check-label" for="exampleCheck1">NZ Wheelbarrows</label>
+						</div>
+						<div class="dropdown-item">
+							<input type="checkbox" class="form-check-input" id="exampleCheck1">
+							<label class="form-check-label" for="exampleCheck1">Troopers</label>
+						</div>
+						<div class="dropdown-item">
+							<input type="checkbox" class="form-check-input" id="exampleCheck1">
+							<label class="form-check-label" for="exampleCheck1">Daltons</label>
+						</div>
+						<div class="dropdown-item">
+							<input type="checkbox" class="form-check-input" id="exampleCheck1">
+							<label class="form-check-label" for="exampleCheck1">Garden Galore</label>
 						</div>
 					</div>
 				</div>
@@ -137,116 +194,20 @@ if (!empty($_GET["action"])) {
 					</div>
 				</div>
 
-				<div class="dropdown mt-2 mb-3">
-					<button class="btn btn-secondary dropdown-toggle btn-block" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-						Soil Drainage
-					</button>
-					<div class="dropdown-menu btn-block" aria-labelledby="dropdownMenuButton">
-						<!-- Can probably put a loop here for soil drainage -->
-						<div class="dropdown-item">
-							<input type="checkbox" class="form-check-input" id="exampleCheck1">
-							<label class="form-check-label" for="exampleCheck1">Fast</label>
-						</div>
-						<div class="dropdown-item">
-							<input type="checkbox" class="form-check-input" id="exampleCheck1">
-							<label class="form-check-label" for="exampleCheck1">Medium</label>
-						</div>
-						<div class="dropdown-item">
-							<input type="checkbox" class="form-check-input" id="exampleCheck1">
-							<label class="form-check-label" for="exampleCheck1">Low</label>
-						</div>
-					</div>
-				</div>
-
-				<div class="dropdown mt-3 mb-3">
-					<button class="btn btn-secondary dropdown-toggle btn-block" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-						Sun Requirement
-					</button>
-					<div class="dropdown-menu btn-block" aria-labelledby="dropdownMenuButton">
-						<!-- Can probably put a loop here for Requirements -->
-						<div class="dropdown-item">
-							<input type="checkbox" class="form-check-input" id="exampleCheck1">
-							<label class="form-check-label" for="exampleCheck1">Sunny</label>
-						</div>
-						<div class="dropdown-item">
-							<input type="checkbox" class="form-check-input" id="exampleCheck1">
-							<label class="form-check-label" for="exampleCheck1">Medium</label>
-						</div>
-						<div class="dropdown-item">
-							<input type="checkbox" class="form-check-input" id="exampleCheck1">
-							<label class="form-check-label" for="exampleCheck1">Shade</label>
-						</div>
-					</div>
-				</div>
-				<!-- <h4 class="mt-2">Maintenance</h4> -->
-				<div class="dropdown mt-3 mb-2">
-					<button class="btn btn-secondary dropdown-toggle btn-block" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-						Maintenance
-					</button>
-					<div class="dropdown-menu btn-block" aria-labelledby="dropdownMenuButton">
-						<!-- Can probably put a loop here for maintenance -->
-						<div class="dropdown-item">
-							<input type="checkbox" class="form-check-input" id="exampleCheck1">
-							<label class="form-check-label" for="exampleCheck1">High</label>
-						</div>
-						<div class="dropdown-item">
-							<input type="checkbox" class="form-check-input" id="exampleCheck1">
-							<label class="form-check-label" for="exampleCheck1">Medium</label>
-						</div>
-						<div class="dropdown-item">
-							<input type="checkbox" class="form-check-input" id="exampleCheck1">
-							<label class="form-check-label" for="exampleCheck1">Low</label>
-						</div>
-					</div>
-				</div>
-
-				<h4 class="mt-2">Max Height</h4>
-				<div class="row">
-					<div class="col-sm">
-						<input type="text" class="product-height-low form-control mb-2 price-products-form" Name="height-low" maxlength="3" size="3" value="Any" />
-					</div>
-					<div class="col-sm-2">
-						<p class="text-center mt-1"> to </p>
-					</div>
-					<div class="col-sm">
-						<input type="text" class="product-height-high form-control mb-2 price-products-form" Name="height-high" maxlength="3" size="3" value="Any" />
-					</div>
-				</div>
-
-				<div class="dropdown mt-2 mb-2">
-					<button class="btn btn-secondary dropdown-toggle btn-block" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-						Growth Rate
-					</button>
-					<div class="dropdown-menu btn-block" aria-labelledby="dropdownMenuButton">
-						<!-- Can probably put a loop here for growth rates -->
-						<div class="dropdown-item">
-							<input type="checkbox" class="form-check-input" id="exampleCheck1">
-							<label class="form-check-label" for="exampleCheck1">Fast</label>
-						</div>
-						<div class="dropdown-item">
-							<input type="checkbox" class="form-check-input" id="exampleCheck1">
-							<label class="form-check-label" for="exampleCheck1">Medium</label>
-						</div>
-						<div class="dropdown-item">
-							<input type="checkbox" class="form-check-input" id="exampleCheck1">
-							<label class="form-check-label" for="exampleCheck1">Slow</label>
-						</div>
-					</div>
-				</div>
 				<input type="submit" value="Apply Filter" class="btnAddAction mt-3 mb-3 btn btn-primary btn-block" />
 			</div>
 
 			<div class="col">
 
 				<?php
-				$product_array = $db_handle->runQuery("SELECT * FROM tree ORDER BY ProductID ASC");
+				$product_array = $db_handle->runQuery("SELECT * FROM gardenproducts ORDER BY ProductID ASC");
 				if (!empty($product_array)) {
 					foreach ($product_array as $key => $value) {
 						?>
 
 						<div class="row-4 mt-3 border bg-light text-center">
 
-							<form method="post" action="products.php?action=add&Code=<?php echo $product_array[$key]["Code"]; ?>">
+							<form method="post" action="garden-products.php?action=add&Code=<?php echo $product_array[$key]["Code"]; ?>">
 
 								<div class="row">
 
@@ -256,7 +217,7 @@ if (!empty($_GET["action"])) {
 
 									<div class="col-lg mt-2 text-left scale-text-center">
 										<h3>
-											<form action="products.php?action=item&Code=<?php echo $product_array[$key]["Code"]; ?>">
+											<form action="garden-products.php?action=item&Code=<?php echo $product_array[$key]["Code"]; ?>">
 												<input type="hidden" name="hidden_code" value=<?php echo $product_array[$key]["Code"]; ?> />
 												<a class="product-title" href="item.php?action=item&Code=<?php echo $product_array[$key]["Code"]; ?>"><?php echo $product_array[$key]["Name"]; ?></a>
 											</form>
